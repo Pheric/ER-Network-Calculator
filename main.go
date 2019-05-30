574acc6227049843f43a86a7041df43ad59200d9
Eric G <16710890+Pheric@users.noreply.github.com>
2019-05-30 00:14:49 -0500

    Modify .gitignore; add main file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port int
+func main() {
+	setupClFlags()
+
+	mux := http.NewServeMux()
+	mux.Handle("/hello", http.HandlerFunc(serveExamplePage))
+
+	fmt.Printf("Now listening on port %d!\n", port)
+	log.Fatalf("error while listening on port %d: %v\nExiting...\n", port, http.ListenAndServe(fmt.Sprintf("127.1:%d", port), mux))
+}
+
+func setupClFlags() {
+	flag.IntVar(&port, "port", 2500, "the port to run the website on")
+
+	flag.Parse()
+}
+
+func serveExamplePage(w http.ResponseWriter, r *http.Request) {
+	if _, err := w.Write([]byte("Hello, World!")); err != nil {
+		log.Printf("error serving example page: %v\n", err)
+	}
+}
